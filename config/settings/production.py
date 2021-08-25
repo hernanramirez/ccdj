90b98486f41c71cc0ed4e40c5c26818b94f57103
J. Hernán Ramírez R <hr@hernanramirez.info>
2021-08-24 20:55:13 -0400

    ajuste

--- config/settings/production.py
+++ config/settings/production.py
@@ -57,7 +57,7 @@ SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
 
 # STATIC
 # ------------------------
-#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+COLLECTFAST_STRATEGY = "django.core.files.storage.FileSystemStorage"
 STATIC_ROOT = env("STATIC_ROOT")
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
